394f0561d3600991e8104f3cfe8fa35d7f726a3b
ABlankSlate <starworksproductions@gmail.com>
2018-05-10 21:59:19 -0500

    yeah

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,3 +11,59 @@ document.addEventListener('DOMContentLoaded', function() {
     });
   }
 });
+
+const path = window.location.pathname.split('/');
+const page = path[path.length-1].split('.')[0];
+
+if(page == '') page = 'index';
+
+switch(page) {
+  case 'index':
+    $('[time-capsule-select] select').on('change', function() {
+      $('[time-capsule-button]').removeAttr('disabled');
+      $('[data-time-selection]').html($(this).val());
+    });
+    $('[time-capsule-button]').on('click', function() {
+      if($(this).attr('disabled')) return;
+      $('[time-capsule-select] select').attr('disabled', '');
+      $('[time-capsule-button]').addClass('is-loading');
+      $('[time-loader-1]').addClass('animated lightSpeedOut');
+      setTimeout(function() {
+        $('[time-loader-1]').hide();
+        $('[time-loader-2]').show();
+        $('[time-loader-2]').addClass('animated lightSpeedIn');
+        setTimeout(function() {
+          window.location.href = './' + $('[data-time-selection]').html() + '.html';
+        }, 1500);
+      }, 500);
+    });
+    animateHourglass('[data-hourglass]', 0);
+    break;
+  default:
+    console.log('No data for this page.');
+    break;
+}
+
+function animateHourglass(element, frame) {
+  frame++;
+  if(frame > 3) frame = 0;
+  switch(frame) {
+    case 0:
+      $(element).attr('data-prefix', 'far');
+      $(element).attr('data-icon', 'hourglass');
+      break;
+    case 1:
+      $(element).attr('data-prefix', 'fas');
+      $(element).attr('data-icon', 'hourglass-start');
+      break;
+    case 2:
+      $(element).attr('data-icon', 'hourglass-half');
+      break;
+    case 3:
+      $(element).attr('data-icon', 'hourglass-end');
+      break;
+  }
+  setTimeout(function() {
+    animateHourglass(element, frame);
+  }, 300);
+}
