fdc7af06b390538cd6e551fe7ff0b1c59c8b0590
ABlankSlate <starworksproductions@gmail.com>
2018-05-15 23:16:13 -0500

    test

--- assets/js/app.js
+++ assets/js/app.js
@@ -342,7 +342,7 @@ function buildHighlightContent(content) {
   if(tagStart > -1 && tagEnd > -1) {
     var original = content.substring((tagStart+11), tagEnd);
     var hlHash = generateHash();
-    var change = '<span class="ml14 subtitle is-5" hl=' + hlHash + '> <span class="text-wrapper"> <span class="letters">' + original + '</span> </span> </span>';
+    var change = '<span class="ml14 subtitle is-5"> <span class="text-wrapper" hl=' + hlHash + '> <span class="letters">' + original + '</span> </span> </span>';
     content = content.replace('<highlight>' + original + '</highlight>', change);
     return buildHighlightContent(content);
   } else {
