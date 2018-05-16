ae74dcf5abd24ef4bc4313d6ba8fee46aca0cb4a
ABlankSlate <starworksproductions@gmail.com>
2018-05-15 21:35:59 -0500

    Switched to Netlify and stopped using the terrible youtube iframe api

--- assets/js/video-bg.js
+++ assets/js/video-bg.js
@@ -1,33 +1,6 @@
-var tag = document.createElement('script');
-tag.src = "https://www.youtube.com/iframe_api";
-var firstScriptTag = document.getElementsByTagName('script')[0];
-firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+var video = document.getElementById('video-bg');
 
-var player;
-function onYouTubeIframeAPIReady() {
-  player = new YT.Player('video-bg', {
-    videoId: 'kUkvaEjtM34',
-    host: 'https://www.youtube-nocookie.com',
-    playerVars: {
-      'rel': 0,
-      'controls': 0,
-      'showinfo': 0
-    },
-    events: {
-      'onReady': onPlayerReady,
-      'onStateChange': onPlayerStateChange
-    }
-  });
-}
-
-function onPlayerReady(event) {
-  document.getElementsByTagName('body')[0].style = '';
-  event.target.playVideo();
-}
-
-function onPlayerStateChange(event) {
-  if(event.data == 0) {
-    document.getElementsByTagName('body')[0].style = 'background: black;';
-    document.getElementById('video-bg').remove();
-  }
+video.onended = function() {
+  document.getElementsByTagName('body')[0].style = 'background: black;';
+  video.remove();
 }
